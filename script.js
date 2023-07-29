9a03b103d61afe6d98c28698e2471ea039a9e095
Ahmed Iqbal Bhatti <ahmad786687iqbal@gmail.com>
2023-07-30 04:53:21 +0500

    Delegation Events in JS

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+const allButtons = document.getElementsByTagName('button');
+const message = document.createElement('div');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
+///////////////////////////////////////
+// Modal window
+///////////////////////////////////////
 const openModal = function (event) {
     event.preventDefault();
 
@@ -33,32 +39,12 @@ document.addEventListener('keydown', function (e) {
         closeModal();
     }
 });
+//////////////////////////////////////////////////////
+/**
+ * Showing Cookies Notification Bar and Removing it.
+ */
+/////////////////////////////////////////////////////
 
-
-////////// DOM ELEMENTS SELECTION APIs
-// console.log(document.documentElement);
-// console.log(document.head);
-// console.log(document.body);
-
-const header = document.querySelector('.header');
-
-const allSections = document.querySelectorAll('.section');
-// console.log(allSections);
-
-// console.log(document.getElementById('section--1'));
-
-// Selector getElementByTagName out put the collection of HTML and
-// update HTML collection whenever element is removed.
-
-const allButtons = document.getElementsByTagName('button');
-// console.log(allButtons);
-
-// console.log(document.getElementsByClassName('btn'));
-
-
-// Creating and Inserting Elements
-
-const message = document.createElement('div');
 message.classList.add('cookie-message');
 // message.textContent = 'We use cookies for improved functionality and analytics.';
 message.innerHTML = 'We use cookies for improved functionality and analytics.' + '<button class="btn btn--close-cookie">Got it!</button>';
@@ -71,7 +57,8 @@ header.prepend(message);
 
 //  ---- Deleting Element
 const cookieCloseBtn = document.querySelector('.btn--close-cookie');
-cookieCloseBtn.addEventListener('click', () => {
+
+cookieCloseBtn.addEventListener('click', function () {
     message.remove();
     // message.parentElement.removeChild(message);
 })
@@ -87,37 +74,11 @@ message.style.height = Number.parseFloat(getComputedStyle(message).height) + 40 
 
 // document.documentElement.style.setProperty('--color-primary', 'orangered');
 
-// ---- Working with attributes
-const logo = document.querySelector('.nav__logo');
-// console.log(logo.alt);
-// console.log(logo.alt);
-// console.log(logo.className);
-
-
-// ----- Non Standard Attributes
-// console.log(logo.designer);
-
-// console.log(logo.setAttribute('company', 'Bankit'));
-// console.log(logo.src);
-//
-// console.log(logo.getAttribute('src'))
-
-const link = document.querySelector('.nav__link--btn');
-// console.log(link.href);
-// console.log(link.getAttribute('href'));
-
-
-// ---- Data Attributes
-
-// console.log(logo.dataset.versionNumber);
-
+////////////////////////////////////////////////
 /**
  * Smooth Scrolling
  */
-
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-
-const section1 = document.querySelector('#section--1');
+////////////////////////////////////////////////
 
 btnScrollTo.addEventListener('click', (event) => {
     /**
@@ -139,60 +100,253 @@ btnScrollTo.addEventListener('click', (event) => {
     //     behavior: "smooth",
     // })
 
+    console.log(section1)
     section1.scrollIntoView({behavior: "smooth"})
 });
 
-const h1 = document.querySelector('h1');
 
-const alertEventH1 = (event) => {
-    alert('Add Event Lister: Great! You are reading the heading: H1');
-
-    // Use the statement below to use event only for once
-    // h1.removeEventListener("mouseenter", alertEventH1);
-}
-
-h1.addEventListener('mouseenter', alertEventH1);
-
-// The another way to use event only for once
-// setTimeout(() => h1.removeEventListener("mouseenter", alertEventH1), 3000);
-
-// h1.onmouseenter = (event)=>{
-//     alert('Add Event Lister: Great! You are reading the heading: H1')
-//
-// }
-
-
+///////////////////////////////////////////////////
 /**
- * Event Propagation
+ * Page Navigation
+ */
+///////////////////////////////////////////////////
+// Navigation without Delegation
+// document.querySelectorAll('.nav__link').forEach(function (element) {
+//     element.addEventListener('click', function (event) {
+//         event.preventDefault();
+//         console.log('LINK');
+//
+//         const elementHref = this.getAttribute('href');
+//         console.log(elementHref)
+//         // console.log(document.querySelector(elementHref))
+//         document.querySelector(elementHref).scrollIntoView({behavior: "smooth"});
+//     })
+// })
+
+///////////////////////////////////////////////////////////////
+// Navigation with Delegation
+/**
+ * Delegation can be done in Two Steps
+ *
+ * Step 1: Add event listener to common parent element.
+ * Step 2: Determine what element originated the event.
  */
 
-const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+document.querySelector('.nav__links').addEventListener('click', function (event) {
+    event.preventDefault();
+    // console.log(event.target)
 
-const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)}, ${randomInt(0, 255)})`
-console.log(randomColor())
+    // Matching Strategy
+    if (event.target.classList.contains('nav__link')) {
+        const sectionId = event.target.getAttribute('href');
+        document.querySelector(sectionId).scrollIntoView({behavior: "smooth"});
+    }
+})
+
+///////////////////////////////////////////
+/**
+ * Operational Tabs
+ * Tabbed Components
+ */
+///////////////////////////////////////////
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+// One way of Adding event on Tabs
+// tabs.forEach(tab => {
+//     tab.addEventListener('click', function (){
+//         console.log('TAB')
+//     })
+// })
 
 
-document.querySelector('.nav__link').addEventListener('click', function (event){
-    this.style.backgroundColor = randomColor();
+// Other way of Adding event on Tabs
 
-    console.log("Nav Link", event.target, event.currentTarget);
-});
-document.querySelector('.nav__links').addEventListener('click',function (event){
-    // console.log('LINK')
-    this.style.backgroundColor = randomColor();
-    console.log("Nav Links", event.target, event.currentTarget);
+tabsContainer.addEventListener('click', function (event) {
+    const clicked = event.target.closest('.operations__tab');
 
-});
-document.querySelector('.nav').addEventListener('click', function (event){
-//     console.log('LINK')
-    this.style.backgroundColor = randomColor();
-    console.log("NAV", event.target, event.currentTarget)
+    // Null check
+    if (!clicked) return;
 
-});
+    // Removing Active class from tabs
+    tabs.forEach(tab => {
+        tab.classList.remove('operations__tab--active');
+    });
+
+    // Adding active class in clicked tab
+    clicked.classList.add('operations__tab--active');
+
+    // Removing active class from the operations Content
+    tabsContent.forEach(tabContent => {
+        tabContent.classList.remove('operations__content--active')
+    });
+
+    // Adding active class in the Operations Content
+    // console.log(clicked.dataset.tab);
+    document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 
 
+})
 
 
+///////////////////////////////////////////
+///////////////////////////////////////////
+///////////////////////////////////////////
+///////////////////////////////////////////
 
 
+////////// DOM ELEMENTS SELECTION APIs
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
 
+
+// console.log(allSections);
+
+// console.log(document.getElementById('section--1'));
+
+// Selector getElementByTagName out put the collection of HTML and
+// update HTML collection whenever element is removed.
+
+// console.log(allButtons);
+
+// console.log(document.getElementsByClassName('btn'));
+
+
+// Creating and Inserting Elements
+//
+// message.classList.add('cookie-message');
+// // message.textContent = 'We use cookies for improved functionality and analytics.';
+// message.innerHTML = 'We use cookies for improved functionality and analytics.' + '<button class="btn btn--close-cookie">Got it!</button>';
+// header.prepend(message);
+// // header.append(message); // this appends will shift the place of append from first to last
+// // header.append(message.cloneNode(true)); // and this will clone the first element and copy at the end.
+// // header.before(message)
+// // header.after(message)
+//
+//
+// //  ---- Deleting Element
+// cookieCloseBtn.addEventListener('click', () => {
+//     message.remove();
+//     // message.parentElement.removeChild(message);
+// })
+//
+//
+// // --- Styles for Cookies
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '104%';
+//
+// // console.log(getComputedStyle(message).alignItems);
+//
+// message.style.height = Number.parseFloat(getComputedStyle(message).height) + 40 + 'px';
+//
+// // document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// ---- Working with attributes
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.alt);
+// console.log(logo.className);
+
+
+// ----- Non Standard Attributes
+// console.log(logo.designer);
+
+// console.log(logo.setAttribute('company', 'Bankit'));
+// console.log(logo.src);
+//
+// console.log(logo.getAttribute('src'))
+
+// const link = document.querySelector('.nav__link--btn');
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
+
+
+// ---- Data Attributes
+
+// console.log(logo.dataset.versionNumber);
+
+///////////////////////////////////////////////////////
+// -------------Event Hanging
+// const h1 = document.querySelector('h1');
+//
+// const alertEventH1 = (event) => {
+//     alert('Add Event Lister: Great! You are reading the heading: H1');
+//
+//     // Use the statement below to use event only for once
+//     // h1.removeEventListener("mouseenter", alertEventH1);
+// }
+//
+// h1.addEventListener('mouseenter', alertEventH1);
+//
+// // The another way to use event only for once
+// // setTimeout(() => h1.removeEventListener("mouseenter", alertEventH1), 3000);
+//
+// // h1.onmouseenter = (event)=>{
+// //     alert('Add Event Lister: Great! You are reading the heading: H1')
+// //
+// // }
+//
+// //////////////////////////////////////////////////////////
+// /**
+//  * Event Propagation
+//  */
+// /////////////////////////////////////////////////////////
+// const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+//
+// const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)}, ${randomInt(0, 255)})`
+// console.log(randomColor())
+//
+//
+// document.querySelector('.nav__link').addEventListener('click', function (event){
+//     this.style.backgroundColor = randomColor();
+//
+//     console.log("Nav Link", event.target, event.currentTarget);
+// });
+// document.querySelector('.nav__links').addEventListener('click',function (event){
+//     // console.log('LINK')
+//     this.style.backgroundColor = randomColor();
+//     console.log("Nav Links", event.target, event.currentTarget);
+//
+// });
+// document.querySelector('.nav').addEventListener('click', function (event){
+// //     console.log('LINK')
+//     this.style.backgroundColor = randomColor();
+//     console.log("NAV", event.target, event.currentTarget)
+//
+// });
+
+//
+// //////////////////////////////////////
+// /**
+//  * DOM Traversing
+//  */
+
+// const h1= document.querySelector('h1');
+//
+// // ------------ Going downwards: Child
+// console.log(h1.querySelectorAll('.highlight'));
+//
+// console.log(h1.childNodes);
+// console.log(h1.children);
+// h1.firstElementChild.style.color = 'white';
+// h1.lastElementChild.style.color = 'orangered';
+//
+//
+// // ------------- Going Upwards: Parents
+//
+// console.log(h1.parentElement);
+// console.log(h1.parentNode);
+// // h1.closest('.header').style.background = 'var(--gradient-secondary)';
+//
+//
+// // -------- Going Sideways: Siblings
+//
+// console.log( h1.previousElementSibling)
+// console.log(h1.nextElementSibling)
+//
+// console.log(h1.previousSibling)
+// console.log(h1.nextSibling);
+//
+// console.log(h1.parentElement.children)
